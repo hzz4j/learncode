d8903cf256ab61a16923e17b15567e3970b7435f
静默 <cau1403090523@gmail.com>
2022-08-08 01:47:26 +0800

    springboot mybatis

---
+++ mybatis/_10_springboot_druid_mybatis/src/main/java/org/hzz/Application.java
@@ -0,0 +1,27 @@
+package org.hzz;
+
+import org.hzz.mapper.EmpMapper;
+import org.hzz.pojo.Emp;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.hzz.mapper")
+public class Application implements CommandLineRunner {
+    @Autowired
+    private EmpMapper empMapper;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World");
+        Emp emp = empMapper.selectByPrimaryKey(1);
+        System.out.println(emp);
+    }
+}
---
+++ mybatis/_10_springboot_druid_mybatis/src/main/java/org/hzz/pojo/Emp.java
@@ -0,0 +1,42 @@
+package org.hzz.pojo;
+
+public class Emp {
+    private Integer id;
+
+    private String username;
+
+    private Integer deptId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", deptId=" + deptId +
+                '}';
+    }
+}
