9746f537257e1287e6b33538409e4f44a46b1878
静默 <cau1403090523@gmail.com>
2022-08-14 18:58:41 +0800

    Create SymmetricTree_101.java

---
+++ algorithm/src/main/java/org/hzz/tree/SymmetricTree_101.java
@@ -0,0 +1,35 @@
+package org.hzz.tree;
+
+/**
+ * 对称二叉树
+ * https://leetcode.cn/problems/symmetric-tree/
+ */
+public class SymmetricTree_101 {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) {
+            return true;
+        }
+        //调用递归函数，比较左节点，右节点
+        return deepCheck(root.left,root.right);
+    }
+
+    private boolean deepCheck(TreeNode left, TreeNode right) {
+        //递归的终止条件是两个节点都为空
+        //或者两个节点中有一个为空
+        //或者两个节点的值不相等
+        if(left == null && right == null){
+            return true;
+        }
+
+        if(left == null || right == null){
+            return false;
+        }
+
+        if(left.val != right.val){
+            return false;
+        }
+        //再递归的比较 左节点的左孩子 和 右节点的右孩子
+        //以及比较  左节点的右孩子 和 右节点的左孩子
+        return deepCheck(left.left,right.right) && deepCheck(left.right,right.left);
+    }
+}
