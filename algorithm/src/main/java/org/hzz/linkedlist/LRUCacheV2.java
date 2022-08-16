19c06e16c67f8abe22f5a007341433e01f5b9a2c
静默 <cau1403090523@gmail.com>
2022-08-16 14:25:06 +0800

    LRU

---
+++ algorithm/src/main/java/org/hzz/linkedlist/LRUCacheV2.java
@@ -0,0 +1,46 @@
+package org.hzz.linkedlist;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCacheV2 {
+    private int capacity;
+    private Map<Integer,Integer> cache;
+
+    public LRUCacheV2(int capacity) {
+        this.capacity = capacity;
+        cache = new LinkedHashMap<Integer,Integer>(1024,0.75f,true){
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                boolean res = size()>capacity;
+                if(res) System.out.println("删除: "+eldest);
+                return res;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.get(key);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key,value);
+    }
+
+    public static void main(String[] args) {
+        LRUCacheV2 lruCache = new LRUCacheV2(2);
+        lruCache.put(1,1);
+        lruCache.put(2,2);
+        lruCache.put(3,3);
+        System.out.println(lruCache.cache);  // 应该输出 {2=2,3=3}
+        lruCache.get(2);
+        lruCache.put(5,5);
+        System.out.println(lruCache.cache); // 应该输出 {2=2,5=5}
+    }
+}
+/**
+ * 删除: 1=1
+ * {2=2, 3=3}
+ * 删除: 3=3
+ * {2=2, 5=5}
+ */
