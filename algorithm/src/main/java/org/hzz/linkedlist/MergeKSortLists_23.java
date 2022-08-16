baa25ecfc267231304225e0fd514d0f40e10d922
静默 <cau1403090523@gmail.com>
2022-08-16 15:08:25 +0800

    Create MergeKSortLists_23.java

---
+++ algorithm/src/main/java/org/hzz/linkedlist/MergeKSortLists_23.java
@@ -0,0 +1,37 @@
+package org.hzz.linkedlist;
+
+public class MergeKSortLists_23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return merge(lists,0,lists.length-1);
+    }
+
+    private ListNode merge(ListNode[] lists,int left,int right){
+        if(left == right) return lists[left];
+        if(left > right) return null;
+
+        int mid = (left + right) >> 1;
+        return mergeTwoList(merge(lists,left,mid),merge(lists,mid+1,right));
+    }
+
+    private ListNode mergeTwoList(ListNode l1,ListNode l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode head = new ListNode(),p = head;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                p.next = l1;  // 直接等于l1
+                l1 = l1.next;
+            }else{
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+
+        if(l1 != null) p.next = l1;
+        if(l2 != null) p.next = l2;
+
+        return head.next;
+    }
+
+}
