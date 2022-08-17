cd3dba9e77b743c5bf3b2bfb20af24cccf4f0098
静默 <cau1403090523@gmail.com>
2022-08-17 19:01:54 +0800

    update

---
+++ jvm/_01_jvm_classload/src/main/java/org/hzz/User.java
@@ -0,0 +1,30 @@
+package org.hzz;
+
+public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
