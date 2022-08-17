d32bdda14037dfd52c419023b3c9e18ca9e91e31
静默 <cau1403090523@gmail.com>
2022-08-17 20:26:12 +0800

    Update TestA.java

--- jvm/_01_jvm_classload/src/main/java/org/hzz/dynamicload/a/TestA.java
+++ jvm/_01_jvm_classload/src/main/java/org/hzz/dynamicload/a/TestA.java
@@ -6,16 +6,25 @@ package org.hzz.dynamicload.a;
 public class TestA {
     public static void main(String[] args) throws Exception {
         ClassLoader classLoader = A.class.getClassLoader();
+        System.out.println(A.class.getName());
         // 类加载 是一个懒加载，只有使用到了才会进行加载
-        Class<?> aClass = classLoader.loadClass("org.hzz.dynamicload.A");
+        Class<?> aClass = classLoader.loadClass("org.hzz.dynamicload.a.A");
+        System.out.println(A.class == aClass);
+        System.out.println(A.class);
         // 初始化
         //aClass.newInstance();
         // A.hello();
     }
 }
+
+/**
+ * org.hzz.dynamicload.a.A
+ * true
+ * class org.hzz.dynamicload.a.A
+ */
 class A {
     static {
-        System.out.println("hello World");
+        System.out.println("A init success");
     }
 
     public static void hello() {
