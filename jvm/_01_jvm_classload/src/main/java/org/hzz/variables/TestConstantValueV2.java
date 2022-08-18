e509249516f7f7ec7adcc28c65ae24e786678914
静默 <cau1403090523@gmail.com>
2022-08-18 16:21:42 +0800

    Update TestConstantValueV2.java

--- jvm/_01_jvm_classload/src/main/java/org/hzz/variables/TestConstantValueV2.java
+++ jvm/_01_jvm_classload/src/main/java/org/hzz/variables/TestConstantValueV2.java
@@ -4,7 +4,11 @@ import java.lang.reflect.Field;
 
 public class TestConstantValueV2 {
     public static void main(String[] args) throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
+        System.out.println(V2.A);
+        System.out.println(V2.class);
         Class<?> v2Class = ClassLoader.getSystemClassLoader().loadClass("org.hzz.variables.V2");
+        System.out.println("-----------------------");
+        // 之前并没有连接
         Field a = v2Class.getField("A");
         System.out.println(a.get(null));
     }
