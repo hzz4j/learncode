9748fe7c93ae5c905083e8acb746523155121201
静默 <cau1403090523@gmail.com>
2022-09-27 22:15:02 +0800

    finished first part

---
+++ vue/read-world-project/small-shopping/src/components/head/GlobalHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- 导航栏 -->
+  <nav class="menus-wrapper">
+    <ul class="container">
+      <li class="menu-item"><a href="javascript:void(0)">测试用户</a></li>
+      <li class="menu-item"><a href="javascript:void(0)">退出登录</a></li>
+      <li class="menu-item"><a href="javascript:void(0)">会员中心</a></li>
+      <li class="menu-item"><a href="javascript:void(0)">帮助中心</a></li>
+      <li class="menu-item"><a href="javascript:void(0)">关于我们</a></li>
+      <li class="menu-item"><a href="javascript:void(0)">手机版</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+// @import "../../assets/styles/_variables";
+.menus-wrapper {
+  background-color: $dark-color1;
+  .container {
+    height: 5.3rem;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    .menu-item {
+      padding: 0 1rem;
+      font-size: 1.6rem;
+      text-align: center;
+      a {
+        color: $gray-color;
+        @include colorText($theme-primary-color);
+
+        &:hover {
+          color: $theme-primary-color;
+        }
+      }
+
+      ~ .menu-item {
+        border-left: 1px solid #666;
+      }
+    }
+  }
+}
+</style>
--- vue/read-world-project/small-shopping/vite.config.ts
+++ vue/read-world-project/small-shopping/vite.config.ts
@@ -12,4 +12,14 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: [
+          `@import "./src/assets/styles/__variables";`,
+          `@import "./src/assets/styles/__mixin";`,
+        ].join(""),
+      },
+    },
+  },
 })
