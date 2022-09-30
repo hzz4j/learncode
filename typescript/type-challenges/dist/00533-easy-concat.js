a9680476789f41e1a4aba9b32ac0653a54391613
静默 <cau1403090523@gmail.com>
2022-10-01 02:18:09 +0800

    finished

---
+++ typescript/type-challenges/dist/00533-easy-concat.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test = [1, 2, 3, 4];
+console.log(test);
---
+++ typescript/type-challenges/src/00533-easy-concat.ts
@@ -0,0 +1,20 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+  Expect<
+    Equal<
+      Concat<["1", 2, "3"], [false, boolean, "4"]>,
+      ["1", 2, "3", false, boolean, "4"]
+    >
+  >
+];
+
+// ============= Your Code Here =============
+type Concat<T extends any[], U extends any[]> = [...T, ...U];
+
+const test: Concat<[1, 2], [3, 4]> = [1, 2, 3, 4];
+console.log(test);
