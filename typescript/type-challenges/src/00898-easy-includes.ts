a4c5f66dc2e438155dd0c49ddf1a8201a6c19487
静默 <cau1403090523@gmail.com>
2022-10-01 02:33:48 +0800

    Create 00898-easy-includes.ts

---
+++ typescript/type-challenges/src/00898-easy-includes.ts
@@ -0,0 +1,28 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Kars">, true>
+  >,
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">, false>
+  >,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>
+];
+
+// ============= Your Code Here =============
+type Includes<T extends readonly any[], U> = U extends T[number] ? true : false;
+
+const MyArray = [
+  { name: "Alice", age: 15 },
+  { name: "Bob", age: 23 },
+  { name: "Eve", age: 38 },
+];
+// constraint array
+type Person = typeof MyArray[number];
