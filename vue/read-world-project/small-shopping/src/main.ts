c9eb3f6529dfa59bbfd762170e14f1e752b6d40b
静默 <cau1403090523@gmail.com>
2022-10-11 18:49:20 +0800

    Update main.ts

--- vue/read-world-project/small-shopping/src/main.ts
+++ vue/read-world-project/small-shopping/src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from "vue"
-import App from "./App.vue"
 import { createPinia } from "pinia"
+
 import "@/assets/styles/common.scss"
+import App from "./App.vue"
 
 const pinia = createPinia()
-
 const app = createApp(App)
+
 app.use(pinia)
 app.mount("#app")
