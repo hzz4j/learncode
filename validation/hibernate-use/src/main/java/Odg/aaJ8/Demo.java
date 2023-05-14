66c21c7da249d63aef1da4dec3e249872b399cb4
静默 <cau1403090523@gmail.com>
2023-05-14 19:50:01 +0800

    finished all

---
+++ validation/hibernate-use/src/main/java/Odg/aaJ8/Demo.java
@@ -0,0 +1,9 @@
+package Odg.aaJ8;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ validation/hibernate-use/src/main/java/javax/learn/Demo.java
@@ -0,0 +1,7 @@
+package javax.learn;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
