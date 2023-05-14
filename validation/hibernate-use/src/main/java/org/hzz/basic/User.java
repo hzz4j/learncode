8655f94dba16c5b902c9e54ec0e483b8afb35a7b
静默 <cau1403090523@gmail.com>
2023-05-14 20:28:23 +0800

    finished all

--- validation/hibernate-use/src/main/java/org/hzz/basic/User.java
+++ validation/hibernate-use/src/main/java/org/hzz/basic/User.java
@@ -3,11 +3,15 @@ package org.hzz.basic;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class User {
-    @NotNull(message = "用户ID不能为空")
-    private String userId;
-    @NotNull
-    private String userName;
+//    @NotNull(message = "用户ID不能为空")
+//    private String userId;
+//    @NotNull
+//    private String userName;
+
+    @Size(min = 1, max = 5, message = "用户密码长度必须在{min}和{max}之间")
+    private String somv = "123456789";
 }
