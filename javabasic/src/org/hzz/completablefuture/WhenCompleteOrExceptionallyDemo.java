601fbc7cfbcb574dda6902da7bccbe5bb231cc74
静默 <cau1403090523@gmail.com>
2023-05-15 00:04:53 +0800

    finshed all

--- javabasic/src/org/hzz/completablefuture/WhenCompleteOrExceptionallyDemo.java
+++ javabasic/src/org/hzz/completablefuture/WhenCompleteOrExceptionallyDemo.java
@@ -2,14 +2,17 @@ package org.hzz.completablefuture;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class WhenCompleteOrExceptionallyDemo {
     public static void main(String[] args) {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
             System.out.println(Thread.currentThread().getName() + "执行有返回结果的任务");
             try {
-                Thread.sleep(5000);
-                if (new Random().nextInt(10) % 2 == 0) {
+                Thread.sleep(1000);
+                int i = new Random().nextInt(10);
+                System.out.println("i = " + i);
+                if (i % 2 == 0) {
                     return 12 / 0;
                 }
             } catch (InterruptedException e) {
@@ -18,22 +21,47 @@ public class WhenCompleteOrExceptionallyDemo {
             return 12;
         });
 
-        future.whenComplete((s, throwable) -> {
+        // 相当于监听器，当future执行完毕后，会回调whenComplete方法
+        CompletableFuture<Integer> whenComplete = future.whenComplete((s, throwable) -> {
             if (throwable == null) {
-                System.out.println("whenComplete->Result: " + s);
+                System.out.println(Thread.currentThread().getName() + ": whenComplete->Result: " + s);
             } else {
-                System.out.println("whenComplete->Exception: " + throwable.getMessage());
+                System.out.println(Thread.currentThread().getName() + ": whenComplete->Exception: " + throwable.getMessage());
             }
         });
 
-        future.exceptionally(throwable -> {
-            System.out.println("exceptionally->Exception: " + throwable.getMessage());
-            System.out.println("exceptionally->异常："+throwable.getMessage()+"，返回默认值");
-            return null;
+        // 相当于监听器，当future执行过程抛出异常，会回调exceptionally方法
+        CompletableFuture<Integer> exceptionally = future.exceptionally(throwable -> {
+            System.out.println(Thread.currentThread().getName() + ": exceptionally->Exception: " + throwable.getMessage());
+            System.out.println(Thread.currentThread().getName() + ": exceptionally->异常：" + throwable.getMessage() + "，返回默认值");
+            return -1;
         });
 
-        Integer result = future.join();
-        System.out.println("Result: " + result);
+        // 如果使用join()方法，会抛出runtime异常，不受检查
+        // 如果不catch会导致main线程退出
+        // Integer result = future.join();
+        // 所以使用get方法比较好
+
+        Integer result = null;
+        try {
+            Thread.sleep(3000);
+            result = future.get();  // 获取不到异常后的返回值，只能获取到异常
+            System.out.println("Result: " + result);
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("main->Exception: " + e.getMessage());
+        }
+
+        try{
+            System.out.println("whenComplete: " + whenComplete.get()); // 和上面一样
+        }catch (InterruptedException | ExecutionException e) {
+            System.out.println("main->Exception: " + e.getMessage());
+        }
+
+        try{
+            System.out.println("exceptionally: " + exceptionally.get()); // 获取到异常后的返回值
+        }catch (InterruptedException | ExecutionException e) {
+            System.out.println("main->Exception: " + e.getMessage());
+        }
         System.out.println("main end");
     }
 }
