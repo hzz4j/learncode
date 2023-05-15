c16cbb967b45eb3b726fe6c960668f6a65976898
静默 <cau1403090523@gmail.com>
2023-05-15 20:46:59 +0800

    finished all

--- javabasic/src/org/hzz/collection/map/TreeMapDemo.java
+++ javabasic/src/org/hzz/collection/map/TreeMapDemo.java
@@ -1,7 +1,25 @@
 package org.hzz.collection.map;
 
+import java.util.*;
+
 public class TreeMapDemo {
     public static void main(String[] args) {
+        Map<String, Integer> map = new TreeMap<>();
+        map.put("orange", 1);
+        map.put("apple", 2);
+        map.put("pear", 3);
+        System.out.println(map); // {apple=2, orange=1, pear=3}
+        orderValueDesc(map);
+    }
 
+    private static void orderValueDesc(Map<String,Integer> map){
+        List<Map.Entry<String, Integer>> list = new ArrayList(map.entrySet());
+
+        // 定义一个比较器Comparator
+        Comparator<Map.Entry<String, Integer>> descValue = (item1,item2)
+                -> item2.getValue().compareTo(item1.getValue());
+
+        list.sort(descValue);
+        System.out.println(list); // [pear=3, apple=2, orange=1]
     }
 }
