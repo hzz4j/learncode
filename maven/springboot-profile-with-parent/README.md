8cf42e3929d0b249daa48e01b89a0ad384dba812
静默 <cau1403090523@gmail.com>
2023-05-28 23:04:40 +0800

    finished all

---
+++ maven/springboot-profile-with-parent/README.md
@@ -0,0 +1,12 @@
+
+```
+mvn clean
+```
+
+```
+mvn spring-boot:run -Pmysql
+```
+
+```
+mvn spring-boot:run -Ph2
+```
---
+++ maven/springboot-profile-with-parent/src/main/java/org/hzz/Application.java
@@ -0,0 +1,20 @@
+package org.hzz;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+    @Value("${server.port}")
+    private String port;
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("run at "+port);
+    }
+}
