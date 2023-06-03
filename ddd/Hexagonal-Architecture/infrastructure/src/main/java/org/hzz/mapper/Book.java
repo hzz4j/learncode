fc2de2ef271fad1e6c1c8a6454ffadeebb561c59
静默 <cau1403090523@gmail.com>
2023-06-03 10:31:48 +0800

    code

---
+++ ddd/Hexagonal-Architecture/infrastructure/src/main/java/org/hzz/mapper/Book.java
@@ -0,0 +1,16 @@
+package org.hzz.mapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+}
